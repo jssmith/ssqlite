7d15d7fdc95b7fc663583a26270e3883d5c68536
Johann Schleier-Smith <jssmith@gmail.com>
2018-01-03 08:19:37 -0800

    avoid segfaults from repeated driver loading

--- dist/handler.py
+++ dist/handler.py
@@ -1,28 +1,80 @@
 import os
+import random
 import socket
 import sys
 import sqlite3
-
 
 import tpcc
 import logging
 from util import *
 from tpccrt import *
 
+lambda_id = "%016x" % random.getrandbits(64)
 efs_location = "%s.efs.%s.amazonaws.com" % (os.environ["EFS_IP"], os.environ["AWS_REGION"])
 database_location = "%s/tpcc-nfs" % socket.gethostbyname(efs_location)
 
+is_ext_loaded = False
+
+def load_nfs4_extension():
+    global is_ext_loaded
+    if not is_ext_loaded:
+        init_conn = sqlite3.connect(":memory:")
+        init_conn.enable_load_extension(True)
+        init_conn.load_extension("./nfs4.so")
+        init_conn.close()
+        is_ext_loaded = True
+
+def test_local():
+    conn = sqlite3.connect("/tmp/test-db")
+    conn.execute('CREATE TABLE IF NOT EXISTS test_write_table (id INT)')
+    conn.execute('INSERT INTO test_write_table (id) VALUES (5)')
+    c = conn.cursor()
+    c.execute('SELECT COUNT(*) FROM test_write_table')
+    num_rows = c.fetchone()[0]
+    print("number of rows in test table", num_rows)
+    conn.commit()
+    conn.close()
+    return { "NumRows": num_rows }
+
 def test_open():
-    init_conn = sqlite3.connect(":memory:")
-    init_conn.enable_load_extension(True)
-    init_conn.load_extension("./nfs4.so")
-    init_conn.close()
+    load_nfs4_extension()
 
     conn = sqlite3.connect(database_location)
     c = conn.cursor()
-    c.execute('SELECT count(*) FROM warehouse')
-    print("number of warehouses", c.fetchone()[0])
+    c.execute('SELECT COUNT(*) FROM warehouse')
+    num_warehouses = c.fetchone()[0]
+    print("number of warehouses", num_warehouses)
     conn.close()
+    return { "NumWarehouses" : num_warehouses }
+
+def test_write():
+    load_nfs4_extension()
+
+    conn = sqlite3.connect(database_location)
+    conn.execute('CREATE TABLE IF NOT EXISTS test_write_table (id INT)')
+    conn.execute('INSERT INTO test_write_table (id) VALUES (5)')
+    c = conn.cursor()
+    c.execute('SELECT COUNT(*) FROM test_write_table')
+    num_rows = c.fetchone()[0]
+    print("number of rows in test table", num_rows)
+    conn.commit()
+    conn.close()
+    return { "NumRows": num_rows }
+
+def test_create():
+    load_nfs4_extension()
+
+    create_database_location = "%s/test-%s" % (socket.gethostbyname(efs_location), lambda_id)
+    conn = sqlite3.connect(create_database_location)
+    conn.execute('CREATE TABLE IF NOT EXISTS test_write_table (id INT)')
+    conn.execute('INSERT INTO test_write_table (id) VALUES (5)')
+    c = conn.cursor()
+    c.execute('SELECT COUNT(*) FROM test_write_table')
+    num_rows = c.fetchone()[0]
+    print("number of rows in test table", num_rows)
+    conn.commit()
+    conn.close()
+    return { "NumRows": num_rows }
 
 def do_tpcc():
     # print(os.environ["NFS_TRACE"])
@@ -47,25 +99,34 @@ def do_tpcc():
         "locking_mode": "exclusive",
         "cache_size": 2000
     }
-    config['reset'] = False
-    config['load'] = False
-    config['execute'] = False
+    config["reset"] = False
+    config["load"] = False
+    config["execute"] = False
     driver.loadConfig(config)
     tpcc.logging.info("Initializing TPC-C benchmark using %s" % driver)
 
     ## Create ScaleParameters
-    scaleParameters = scaleparameters.makeWithScaleFactor(args['warehouses'], args['scalefactor'])
+    scaleParameters = scaleparameters.makeWithScaleFactor(args["warehouses"], args["scalefactor"])
     nurandx = rand.setNURand(nurand.makeForLoad())
 
-    e = executor.Executor(driver, scaleParameters, stop_on_error=args['stop_on_error'])
+    e = executor.Executor(driver, scaleParameters, stop_on_error=args["stop_on_error"])
     driver.executeStart()
-    results = e.execute(args['duration'])
+    results = e.execute(args["duration"])
     driver.executeFinish()
-    return results
+    return results.show()
 
 
 def lambda_handler(event, context):
+    print("lambda id", lambda_id)
     print("database location", database_location)
-    test_open()
-    #print(do_tpcc().show())
-    return "done"
+    tests = {
+        "local": test_local,
+        "open": test_open,
+        "write": test_write,
+        "create": test_create,
+        "tpcc": do_tpcc }
+    res = tests[event["test"]]()
+    return {
+        "LambdaId": lambda_id,
+        "AwsRequestId": context.aws_request_id,
+        "Result": res }
