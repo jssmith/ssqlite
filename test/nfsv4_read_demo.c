af24e9a18d1e8197318de3d7780e674764f0d9f1
scottnuma <scott.numamoto@gmail.com>
2018-11-12 06:28:20 +0000

    Split benchmark writes into blocks

--- test/nfsv4_read_demo.c
+++ test/nfsv4_read_demo.c
@@ -3,46 +3,51 @@
 #include <stdlib.h>
 #include <assert.h>
 
- int main(int argc, char **argv) {
-     nfs4 client;
-     char *server;
-     char *filename = "/writer2.txt";
-     int block_size = 100;
-     int num_blocks = 100;
-     int num_bytes = block_size * num_blocks;
+int main(int argc, char **argv) {
+   nfs4 client;
+   char *server;
+   char *filename = "/writer3.txt";
+   int block_size = 100;
+   char block_content[block_size];
 
-     // create a nfs4 client
-     server = getenv("NFS4_SERVER");
-     if (server == NULL) {
-        printf("Failed to read NFS4_SERVER from environment");
-        exit(1);
-     }
+   for (int i = 0; i < block_size; i++) {
+     block_content[i] = 'a' + i % 26;
+   }
+   int num_blocks = 100;
+   int num_bytes = block_size * num_blocks;
 
-    if (nfs4_create(server, &client)) {
-        printf ("open client fail %s\n", nfs4_error_string(client));
-        exit(1);
-    }
+   // create a nfs4 client
+   server = getenv("NFS4_SERVER");
+   if (server == NULL) {
+      printf("Failed to read NFS4_SERVER from environment");
+      exit(1);
+   }
 
-    // open a file via nfs4
-    struct nfs4_properties p;
-    p.mask = NFS4_PROP_MODE; // TODO: need to understand this flag
-    p.mode = 0666; // TODO
-    nfs4_file f;
-    
-    if (nfs4_open(client, filename, NFS4_WRONLY, &p, &f) != NFS4_OK) {// TODO: file path
-        printf("Failed to open %s\n", filename);
-        exit(1);
-    } 
-    
-    // read a file via nfs4
-    assert(f != NULL);
+  if (nfs4_create(server, &client)) {
+      printf ("open client fail %s\n", nfs4_error_string(client));
+      exit(1);
+  }
 
-    int write_status = nfs4_pwrite(f, (void*) "some words", 0, 6);
-    if (write_status != NFS4_OK) { 
-        printf("Failed to write to %s\n", filename);
-        exit(1);
-    } 
+  // open a file via nfs4
+  struct nfs4_properties p;
+  p.mask = NFS4_PROP_MODE; // TODO: need to understand this flag
+  p.mode = 0666; // TODO
+  nfs4_file f;
+  
+  if (nfs4_open(client, filename, NFS4_RDWRITE | NFS4_CREAT, &p, &f) != NFS4_OK) {
+      printf("Failed to open %s\n", filename);
+      exit(1);
+  } 
+  assert(f != NULL);
 
-    return 0;
- }
+  for (int bytes_written = 0; bytes_written < num_bytes; bytes_written += block_size) {
+      int write_status = nfs4_pwrite(f, (void*) block_content, bytes_written, block_size);
+      if (write_status != NFS4_OK) { 
+          printf("Failed to write to %s\n", filename);
+          exit(1);
+      } 
+  }
+
+  return 0;
+}
  
