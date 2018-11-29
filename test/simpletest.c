6739a1593bce9567a1a52332940f7cb8344efb51
Johann Schleier-Smith <jssmith@gmail.com>
2018-11-28 16:06:14 -0800

    cleanup python demo

--- test/simpletest.c
+++ test/simpletest.c
@@ -25,38 +25,33 @@ void create_client_py(char* host_ip)
 }
 
 void freeFile(PyObject *obj) {
-  free(PyCapsule_GetPointer(obj, "File"));
+    free(PyCapsule_GetPointer(obj, "File"));
 }
 
 // Open file_name into nfs4_file f with read only access.
 PyObject *open_file_py(char* file_name) {
-  printf("open file\n");
-  struct nfs4_properties p;
-  p.mask = NFS4_PROP_MODE; 
-  p.mode = 0666; 
+    struct nfs4_properties p;
+    p.mask = NFS4_PROP_MODE;
+    p.mode = 0666;
 
-  nfs4_file f = malloc(sizeof(struct nfs4_file));
-  if (nfs4_open(client, file_name, NFS4_RDONLY, &p, &f) != NFS4_OK) {
-    printf("Failed to open %s:%s\n", file_name, nfs4_error_string(client));
-    exit(1);
-  }
-  assert(f != NULL);
-  PyObject *obj = PyCapsule_New(f, "File", &freeFile);
-  printf("Create capsule at %x\n");
-  return obj;
+    nfs4_file f = malloc(sizeof(struct nfs4_file));
+    if (nfs4_open(client, file_name, NFS4_RDONLY, &p, &f) != NFS4_OK) {
+        printf("Failed to open %s:%s\n", file_name, nfs4_error_string(client));
+        exit(1);
+    }
+    assert(f != NULL);
+    PyObject *obj = PyCapsule_New(f, "File", &freeFile);
+    return obj;
 }
 
 void read_file_py(PyObject *obj, int size) {
-  printf("Receive capsule at %x\n");
-  int is_valid = PyCapsule_IsValid(obj, "File");
-  printf("capsule validity %d", is_valid);
-  nfs4_file f = PyCapsule_GetPointer(obj, "File");
-  void *read_dst = calloc(size + 1, 1); 
-  int read_status = nfs4_pread(f, read_dst, 0, size);
-  if (read_status != NFS4_OK) { 
-    printf("Failed to read file:%s\n", nfs4_error_string(client));
-    exit(1);
-  } 
-  printf("---content---\n%s\n-------------\n", (char*) read_dst);
-  free(read_dst);
+    nfs4_file f = PyCapsule_GetPointer(obj, "File");
+    void *read_dst = calloc(size + 1, 1);
+    int read_status = nfs4_pread(f, read_dst, 0, size);
+    if (read_status != NFS4_OK) {
+        printf("Failed to read file:%s\n", nfs4_error_string(client));
+        exit(1);
+    }
+    printf("---content---\n%s\n-------------\n", (char*) read_dst);
+    free(read_dst);
 }
