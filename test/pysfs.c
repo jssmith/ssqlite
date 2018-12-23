0b326c8c8523da7fd1cc431f00436d4964713feb
Libo Chen <clb729@gmail.com>
2018-12-23 03:31:45 +0000

    segfault on PyErr_SetString

--- test/pysfs.c
+++ test/pysfs.c
@@ -37,6 +37,10 @@ PyObject *open_file_py(const char* file_name, const char* mode) {
     nfs4_file f = malloc(sizeof(struct nfs4_file));
     int error_code;
     if ((error_code = nfs4_open(client, file_name, flags, &p, &f)) != NFS4_OK) {
+        if (error_code == -NFS4_ENOENT) {
+            PyErr_SetString(PyExc_FileNotFoundError, "File does not exist");
+            return (PyObject *) NULL; 
+        }
         printf("Failed to open %s: %s\n", file_name, nfs4_error_string(client));
         exit(1);
     }
