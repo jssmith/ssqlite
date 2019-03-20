bb9d5e5c9cc7a4c71b93c1065163ac79907791f0
scottnuma <scott.numamoto@gmail.com>
2019-03-20 19:06:04 +0000

    Use prefixed sfs importation

--- dist/grep_handler.py
+++ dist/grep_handler.py
@@ -2,13 +2,13 @@ import os
 import re
 import sys
 
-from sfs import *
 
+import sfs
 
 def grep(file_name, phrase):
     p = re.compile(phrase)
     matched_lines = []
-    with open(file_name, mode='r') as f:
+    with sfs.open(file_name, mode='r') as f:
         for line in f:
             if p.search(line):
                 matched_lines.append(file_name + ": " + line)
@@ -17,7 +17,7 @@ def grep(file_name, phrase):
 
 def lambda_handler(event, context):
     mount_point = os.environ["NFS4_SERVER"]
-    mount(mount_point)
+    sfs.mount(mount_point)
     print("event", event)
     return {
         "matches": grep(event["file_path"], event["phrase"]),
