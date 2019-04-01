706d1d0b94f4e257c106f54fae0c1b3db54c8bc4
scottnuma <scott.numamoto@gmail.com>
2019-04-01 13:54:40 -0700

    Configure use of SFS for grep_handler

--- dist/grep_handler.py
+++ dist/grep_handler.py
@@ -2,24 +2,41 @@ import os
 import re
 import sys
 
-
 import sfs
 
-def grep(file_name, phrase):
+
+def grep(file_name, phrase, use_sfs=True):
     p = re.compile(phrase)
     matched_lines = []
-    with sfs.open(file_name, mode='r') as f:
-        for line in f:
+
+    if use_sfs:
+        file = sfs.open(file_name, mode='r')
+    else:
+        file = open(file_name, mode='r')
+
+    try:
+        for line in file:
             if p.search(line):
                 matched_lines.append(file_name + ": " + line)
+    finally:
+        file.close()
+
     return matched_lines
 
 
 def lambda_handler(event, context):
-    mount_point = os.environ["NFS4_SERVER"]
-    sfs.mount(mount_point)
-    print("event", event)
+    """
+    Adapt grep for usage by AWS Lambda.
+
+    context: if non-nil uses SFS
+    """
+
+    use_sfs = context is not None
+    if use_sfs:
+        mount_point = os.environ["NFS4_SERVER"]
+        sfs.mount(mount_point)
+
     return {
-        "matches": grep(event["file_path"], event["phrase"]),
+        "matches": grep(event["file_path"], event["phrase"], use_sfs),
         "file_name": event["file_path"],
     }
