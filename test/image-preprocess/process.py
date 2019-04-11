4f565c442dac24fb9f69a6ea3002c0bd695e1f0f
scottnuma <scott.numamoto@gmail.com>
2019-04-11 05:32:04 +0000

    Fix imports of processing images
    - Add testing of process.py

--- test/image-preprocess/process.py
+++ test/image-preprocess/process.py
@@ -1,20 +1,21 @@
-import PIL
+from PIL import ImageEnhance, Image
+import io
 import sfs
 
 DEFAULT_FILTERS = [
-    (PIL.ImageEnhance.Color, 0.0),
-    (PIL.ImageEnhance.Contrast, 1.5),
-    (PIL.ImageEnhance.Sharpness, 2.0),
+    (ImageEnhance.Color, 0.0),
+    (ImageEnhance.Contrast, 1.5),
+    (ImageEnhance.Sharpness, 2.0),
 ]
 
 
 def process_image(input_file, output_file, filters, local):
     """Apply series of filters to input_file and record changes in output_file."""
     if local:
-        image = PIL.Image.open(input_file)
+        image = Image.open(input_file)
     else:
         image_file = sfs.open(input_file, "rb")
-        image = PIL.Image.open(image_file)
+        image = Image.open(image_file)
 
     for enhancer, factor in filters:
         image = enhancer(image).enhance(factor)
@@ -33,3 +34,16 @@ def process_image_arguments(input_folder, output_folder, filters, local, files):
         input_file = "{}/{}".format(input_folder, file_name)
         output_file = "{}/{}".format(output_folder, file_name)
         yield (input_file, output_file, filters, local)
+
+if __name__ == "__main__":
+    import os
+    mount_point = os.environ["NFS4_SERVER"]
+    sfs.mount(mount_point)
+    sfs_file = sfs.open("/cat.jpg", "rb")
+    reg_file = open("/efs/cat.jpg", "rb")
+    sfs_data = sfs_file.read()
+    reg_data = reg_file.read()
+    print(len(sfs_data))
+    print(len(reg_data))
+
+
