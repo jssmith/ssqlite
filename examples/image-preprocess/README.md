ff42f76c8db02935f6b62bd7950c0eb927e86b62
scottnuma <scott.numamoto@gmail.com>
2019-05-11 08:32:52 +0000

    Package up image-preprocess up nicely

---
+++ examples/image-preprocess/README.md
@@ -0,0 +1,34 @@
+# Image Preprocessing
+
+Image Preprocessing allows users to preprocess images in parallel.
+
+## Metrics 
+
+`to be a nice graph comparison`
+
+## Installation
+
+Install Pillow to this folder
+
+```
+pip install --target . Pillow
+```
+
+Create a lambda function with a name such as `sfs-image-preprocess`
+Configure execution role.
+Configure VPC, subnets, and security groups.
+Configure memory and timeout.
+Configure environment variables
+        NFS4_SERVER
+        LOGLEVEL
+
+Create a s3 bucket
+
+Upload the code with `deploy.sh`. Make sure you have the necessary permissions 
+and environment variables.
+
+In the console, change the handler function to `handler.lambda_handler`
+
+Create a test event
+
+Test your function
--- examples/image-preprocess/handler.py
+++ examples/image-preprocess/handler.py
@@ -1,1 +1,38 @@
-../../dist/handler.py
+import os
+
+import process
+import sfs
+import logging
+
+def setup_logging():
+    """Setup logging that outputs to Cloudwatch."""
+    root = logging.getLogger()
+    if root.handlers:
+        for handler in root.handlers:
+            root.removeHandler(handler)
+
+    logging.basicConfig(
+            level=os.environ.get("LOGLEVEL", "DEBUG"),
+            format="%(asctime)s\t%(levelname)s\t%(funcName)s\t%(message)s")
+    logging.info("starting")
+
+def lambda_handler(event, context):
+    input_files = event["input_files"]
+    output_files = event["output_files"]
+    logging.info("input_files: %s", input_files)
+    logging.info("output_files: %s", output_files)
+
+    process.process_images(
+        input_files, output_files, process.DEFAULT_FILTERS, False)
+    logging.info("finished")
+
+setup_logging()
+
+mount_point = os.environ["NFS4_SERVER"]
+sfs.mount(mount_point)
+
+if __name__ == "__main__":
+    lambda_handler({
+        "input_files": ["/cat.jpg"],
+        "output_files": ["/a_cat.jpg"]},
+        None)
